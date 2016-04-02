2296199226c888bd4a12b2afb57b78872bd49d61
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2016-04-02 02:13:09 -0700

    Initial commit - a console app that outputs the version string

---
+++ src/main/java/org/mcmonkey/denizen2console/Denizen2Console.java
@@ -0,0 +1,10 @@
+package org.mcmonkey.denizen2console;
+
+import org.mcmonkey.denizen2core.Denizen2Core;
+
+public class Denizen2Console {
+
+    public static void main(String[] args) {
+        System.out.println(Denizen2Core.version);
+    }
+}
